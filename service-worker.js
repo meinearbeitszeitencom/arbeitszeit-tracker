3c0e8fd4aefb25c34101874ffdeca4042622f874
meinearbeitszeitencom <kontakt.meinearbeitszeit@gmail.com>
2026-01-02 18:27:32 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = "arbeitszeit-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/logo.svg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
