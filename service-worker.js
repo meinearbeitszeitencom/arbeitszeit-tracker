d5de0be5364d36633909d4d747ed111d8cc99396
meinearbeitszeitencom <kontakt.meinearbeitszeit@gmail.com>
2026-01-04 16:01:04 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,37 @@
-const CACHE = "arbeitszeit-v2";
-const FILES = [
+const CACHE_NAME = "meinearbeitszeit-v4";
+const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/logo.svg"
+  "/logo.svg",
+  "/manifest.json"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
 });
